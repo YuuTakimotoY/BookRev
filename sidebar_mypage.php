6df9d5ebba15a37c35837781732138dec87a0f0d
YuuTakimoto <yuu9701612038@gmail.com>
2019-08-29 20:53:13 +0900

    gestlogin add

--- sidebar_mypage.php
+++ sidebar_mypage.php
@@ -3,7 +3,7 @@
   <a href="profEdit.php">プロフィール編集</a>
   <a href="passEdit.php">パスワード変更</a>
   <?php
-  if($_SESSION['user_id'] = 22){
+  if($_SESSION['user_id'] == 22){
 
   }else{?>
     <a href="withdraw.php">退会</a>
